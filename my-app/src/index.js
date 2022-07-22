310eb1b5b5a592b532682b27c9717dfcd2331846
NeilGGopal <gopaln@purdue.edu>
2022-07-21 17:51:37 -0700

    Added Page render in index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,16 @@
+/* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Header } from './stories/Header';
+import { Page } from './stories/Page';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Page />
   </React.StrictMode>
 );
 
